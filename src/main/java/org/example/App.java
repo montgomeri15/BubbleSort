a63e5d3862c0fab2bd4d03610ff71696c097c1f1
montgomeri15 <montgomeri15@gmail.com>
2022-11-14 19:33:50 +0200

    Bubble sort with tests

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,8 +3,21 @@ package org.example;
 import java.util.Arrays;
 
 public class App {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        //Сортування бульбашкою
+        int[] sortArr = {12, 6, 4, 1, 15, 10};
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.sort(sortArr);
 
+        //Сортування бульбашкою 2D-масиву
+        int[][] sortMatrix = new int[][]{
+                {8, 3, 5},
+                {1, 4, 6},
+                {9, 7, 2}
+        };
+        MatrixBubbleSort matrixBubbleSort = new MatrixBubbleSort();
+        matrixBubbleSort.sort(sortMatrix);
+    }
 }
 
 
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -5,5 +5,35 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AppTest {
+    @Test
+    void testBubbleSort(){
+        int[] testSortArr = {12, 6, 4, 1, 15, 10};
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] sortedArr = bubbleSort.sort(testSortArr);
 
+        assertThat(sortedArr).isEqualTo(new int[]{1, 4, 6, 10, 12, 15});
+        assertThat(sortedArr).isNotEqualTo(new int[]{12, 6, 4, 1, 15, 10});
+    }
+
+    @Test
+    void testMatrixBubbleSort(){
+        int[][] sortMatrix = new int[][]{
+                {8, 3, 5},
+                {1, 4, 6},
+                {9, 7, 2}
+        };
+        MatrixBubbleSort matrixBubbleSort = new MatrixBubbleSort();
+        int[][] sortedMatrix = matrixBubbleSort.sort(sortMatrix);
+
+        assertThat(sortedMatrix).isEqualTo(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        });
+        assertThat(sortedMatrix).isNotEqualTo(new int[][]{
+                {8, 3, 5},
+                {1, 4, 6},
+                {9, 7, 2}
+        });
+    }
 }
